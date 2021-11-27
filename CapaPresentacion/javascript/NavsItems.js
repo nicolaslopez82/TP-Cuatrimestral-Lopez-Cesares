dcab684b7f32af0568c82d6fb4dc7803f2401c11
nicolaslopez82 <nicolaslopez82@gmail.com>
2021-11-27 19:55:30 -0300

    nav item active not solution yet

--- CapaPresentacion/javascript/NavsItems.js
+++ CapaPresentacion/javascript/NavsItems.js
@@ -1,22 +1,8 @@
-////// evento click para boton eliminar
-////$(document).on('click', '.nav-link', function (e) {
-////  e.preventDefault();
-////  // remover clase activa de cualquier nav-item.
-////  $('.nav-link').removeClass('active')
-////  // agregar clase active al item.
-////  $(this).closest('.nav-link').addClass('active')
-////});
-
 // evento click para boton eliminar
-function activeLinkControl() { 
-  $('.nav-item').click(function () {
+$(document).on('click', '.nav-link', function (e) {
+ // e.preventDefault();
   // remover clase activa de cualquier nav-item.
-  $('.nav-item').removeClass('active')
+  $('.nav-link').removeClass('active')
   // agregar clase active al item.
-  $(this).closest('.nav-item').addClass('active')
-});
-}
-
-$(document).ready(function () {
-  activeLinkControl()
+  $(this).addClass('active');
 });
