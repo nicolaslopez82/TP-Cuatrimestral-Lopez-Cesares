052f0adfafda13be2de4081040a72dc6228d4b98
nicolaslopez82 <nicolaslopez82@gmail.com>
2021-12-05 01:56:28 -0300

    SPs Especialidades Medicos HorDisp Reservas Maximo Cesare

--- DB-Script/dml/store-procedures/TPC_Clinica-storeprocedures.sql
+++ DB-Script/dml/store-procedures/TPC_Clinica-storeprocedures.sql
@@ -1,3 +1,5 @@
+--///////////////////////////////////// USUARIOS ///////////////////////////////
+
 /****** Object:  StoredProcedure [dbo].[SP_AgregarUsuario] ******/
 SET ANSI_NULLS ON
 GO
@@ -138,6 +140,27 @@ BEGIN
 END
 GO
 
+/****** Object:  StoredProcedure [dbo].[SP_ListarUsuarios] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[SP_ListarUsuarios]
+AS
+BEGIN
+	SELECT [idUsuario]
+      ,[idTipoUsuario]
+      ,[nombre]
+      ,[apellido]
+      ,[nroDocumento]      
+      ,[usuario]      
+	FROM [TPC_Clinica].[dbo].[Usuario]
+	WHERE estado = 1
+END
+GO
+
+--///////////////////////////////////// PACIENTES ///////////////////////////////
+
 /****** Object:  StoredProcedure [dbo].[SP_ListarPacientes] ******/
 SET ANSI_NULLS ON
 GO
@@ -228,21 +251,385 @@ AS
 	END
 GO
 
-/****** Object:  StoredProcedure [dbo].[SP_ListarUsuarios] ******/
+--///////////////////////////////////// ESPECIALIDAD ///////////////////////////////
+
+/* Insertar desde aqui INICIO */
+/****** Object:  StoredProcedure [dbo].[SP_ListarEspecialidad] ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE [dbo].[SP_ListarUsuarios]
-AS
+CREATE PROCEDURE [dbo].[SP_ListarEspecialidad]
+AS 
 BEGIN
-	SELECT [idUsuario]
-      ,[idTipoUsuario]
-      ,[nombre]
-      ,[apellido]
-      ,[nroDocumento]      
-      ,[usuario]      
-	FROM [TPC_Clinica].[dbo].[Usuario]
+	SELECT 	E.idEspecialidad,
+			E.descripcion,
+			E.estado
+	
+	FROM Especialidad AS E
 	WHERE estado = 1
+
 END
 GO
+
+/****** Object:  StoredProcedure [dbo].[SP_AgregarEspecialidad] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[SP_AgregarEspecialidad]
+(
+@Descripcion VARCHAR(25)
+)
+AS
+	BEGIN
+		INSERT INTO Especialidad (descripcion, estado)
+		VALUES (@Descripcion,1)
+	END
+GO
+
+/****** Object:  StoredProcedure [dbo].[SP_EliminarEspecialidad] ******/
+--Eliminar Especialidad (Baja logica dependiente del Trigger)
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[SP_EliminarEspecialidad]
+(
+	@idEspecialidad int
+)
+AS 
+BEGIN
+ DELETE FROM Especialidad WHERE idEspecialidad = @idEspecialidad
+
+END
+GO
+--drop PROCEDURE [SP_ElimiarEspecialidad]
+
+/****** Object:  Trigger [dbo].[TR_BajaLogicaEspecialidad] ******/
+--Trigger Delete Especialidad
+GO
+CREATE TRIGGER TR_BajaLogicaEspecialidad ON Especialidad
+INSTEAD of DELETE
+AS 
+BEGIN
+
+DECLARE @idEspecialidadEntrante INT
+SELECT @idEspecialidadEntrante = idEspecialidad FROM deleted
+
+DECLARE @EstadoEntrante BIT
+SELECT @EstadoEntrante = estado FROM Especialidad WHERE idEspecialidad = @idEspecialidadEntrante
+
+	if(@EstadoEntrante = 0)
+		BEGIN
+			DELETE FROM Especialidad WHERE idEspecialidad = @idEspecialidadEntrante
+		END
+	ELSE
+		BEGIN
+			UPDATE Especialidad SET estado = 0 WHERE idEspecialidad = @idEspecialidadEntrante
+		END
+END
+GO
+
+/****** Object:  StoredProcedure [dbo].[SP_ModificarDescripcion] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[SP_ModificarDescripcion]
+(
+	@idEspecialidad INT,
+	@DescripcionNew VARCHAR(25)
+)
+AS 
+BEGIN
+	UPDATE Especialidad SET descripcion = @DescripcionNew WHERE idEspecialidad = @idEspecialidad
+END
+GO
+
+--///////////////////////////////////// MEDICOS ///////////////////////////////
+
+/****** Object:  StoredProcedure [dbo].[SP_InsertarMedico] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[SP_InsertarMedico] 
+(
+@idUsuarioEntrante BIGINT,
+@idEspecialidadEntrante BIGINT
+)
+AS
+BEGIN
+
+	IF(SELECT count(*) FROM Medico WHERE idUsuario = @idEspecialidadEntrante) > 0
+		BEGIN
+			PRINT 'Este usuario ya está registrado como medico'
+		END
+	ELSE
+		BEGIN
+			IF(SELECT count(*) FROM Usuario WHERE idUsuario = @idUsuarioEntrante) > 0
+				BEGIN
+						INSERT INTO Medico (idUsuario,estado)
+						VALUES(@idUsuarioEntrante,1)
+
+						DECLARE @idmediconuevo BIGINT
+						SELECT @idmediconuevo = m.idMedico FROM Medico AS m WHERE m.idUsuario = @idUsuarioEntrante
+
+						INSERT INTO MedicoEspecialidad(idEspecialidad,idMedico)
+						VALUES(@idEspecialidadEntrante,@idmediconuevo)
+				END
+			ELSE
+				BEGIN
+					PRINT 'Usuario no registrado'
+				END
+		END
+END
+
+/****** Object:  StoredProcedure [dbo].[SP_EliminarMedico] ******/
+-- Eliminar Medico o baja logica dependiente del Trigger.
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[SP_EliminarMedico] 
+(
+@idMedicoEntrante BIGINT
+)
+AS
+BEGIN
+	IF(SELECT count(*) FROM Medico WHERE idMedico = @idMedicoEntrante) > 0
+		BEGIN
+			DELETE FROM Medico WHERE idMedico = @idMedicoEntrante
+		END
+	ELSE
+		BEGIN
+			PRINT 'Medico Inexistente'
+		END
+END
+
+
+/****** Object:  Trigger [dbo].[TR_BajaLogicaMedico] ******/
+
+--Trigger Delete Medico
+/*
+GO
+CREATE TRIGGER TR_BajaLogicaMedico on Medico
+INSTEAD of DELETE
+AS
+BEGIN
+DECLARE @idMedicoEntrante BIGINT
+SELECT @idMedicoEntrante = idMedico FROM deleted
+
+DECLARE @EstadoEntrante BIT 
+SELECT @EstadoEntrante = estado FROM Medico WHERE idMedico = @idMedicoEntrante
+
+	if(@EstadoEntrante = 0)
+		BEGIN
+			DELETE FROM Medico WHERE idMedico = @idMedicoEntrante
+		END
+	ELSE
+		BEGIN
+			UPDATE Medico SET estado = 0 WHERE idMedico = @idMedicoEntrante
+		END	
+END
+*/
+
+
+/****** Object:  StoredProcedure [dbo].[SP_DarAltaMedico] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create PROCEDURE [dbo].[SP_DarAltaMedico]
+(
+	@idMedicoEntrante BIGINT
+)
+AS
+BEGIN
+	IF(SELECT count(*) FROM Medico WHERE idMedico = @idMedicoEntrante) > 0
+		BEGIN
+			UPDATE medico SET estado = 1 WHERE idMedico = @idMedicoEntrante
+		END
+	ELSE
+		BEGIN
+			PRINT 'Medico Inexistente'
+		END
+END
+
+/****** Object:  StoredProcedure [dbo].[SP_ListaMedicos] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[SP_ListaMedicos]
+AS
+	BEGIN
+		SELECT 
+		M.idMedico,
+		U.apellido,
+		U.nombre,
+		U.nroDocumento,
+		ME.idEspecialidad,
+		E.descripcion
+		
+		FROM Medico AS M
+		INNER JOIN MedicoEspecialidad AS ME ON ME.idMedico = M.idMedico
+		INNER JOIN Usuario AS U ON U.idUsuario = M.idUsuario 
+		INNER JOIN Especialidad as E ON E.idEspecialidad = ME.idEspecialidad
+		WHERE M.estado = 1		
+	END
+GO
+
+/****** Object:  StoredProcedure [dbo].[SP_ListaMedicosDadosDeBaja] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[SP_ListaMedicosDadosDeBaja]
+AS
+	BEGIN
+		SELECT 
+		M.idMedico,
+		U.apellido,
+		U.nombre,
+		U.nroDocumento,
+		ME.idEspecialidad,
+		E.descripcion
+		
+		FROM Medico AS M
+		INNER JOIN MedicoEspecialidad AS ME ON ME.idMedico = M.idMedico
+		INNER JOIN Usuario AS U ON U.idUsuario = M.idUsuario 
+		inner join Especialidad AS E ON E.idEspecialidad = ME.idEspecialidad
+		WHERE M.estado = 0
+	END
+
+
+
+--///////////////////// HORARIOS DISPONIBLES ///////////////////////////////////
+
+/****** Object:  StoredProcedure [dbo].[SP_ListaHorarioDisponible] ******/
+--Lista Horario Disponible por id de Medico
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[SP_ListaHorarioDisponible]
+(
+	@idmedico BIGINT
+)
+AS
+BEGIN
+
+	SELECT idHorarioDisponible,
+		   fechaHorarioDisponible
+	FROM HorarioDisponible WHERE idMedico = @idmedico AND estado = 1
+
+END
+
+/****** Object:  StoredProcedure [dbo].[SP_BajaHorario] ******/
+-- Dar de baja al horario disponible
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[SP_BajaHorario]
+(
+	@idhorario BIGINT
+)
+AS
+BEGIN
+	UPDATE HorarioDisponible SET estado = 0 WHERE idHorarioDisponible = @idhorario 
+END
+
+/****** Object:  StoredProcedure [dbo].[SP_AltaHorario] ******/
+-- Dar de Alta al Horario Disponible
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[SP_AltaHorario]
+(
+	@idhorario bigint
+)
+AS 
+BEGIN
+	UPDATE HorarioDisponible SET estado = 1 WHERE idHorarioDisponible = @idhorario 
+END
+
+/****** Object:  StoredProcedure [dbo].[SP_AgregarHorario] ******/
+-- Agregar un Horario Disponible x id de Medico
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[SP_AgregarHorario]
+(
+	@idmedico BIGINT,
+	@horarionuevo DATETIME
+)
+AS 
+BEGIN
+	INSERT INTO HorarioDisponible (idMedico,fechaHorarioDisponible,estado)
+	VALUES (@idmedico,@horarionuevo,1)
+END
+
+/****** Object:  StoredProcedure [dbo].[SP_GetHorario] ******/
+-- Obtener Horario Disponible
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[SP_GetHorario]
+(
+	@idhorario BIGINT
+)
+AS 
+BEGIN
+ SELECT fechaHorarioDisponible FROM HorarioDisponible WHERE idHorarioDisponible = @idhorario
+END
+
+--///////////////////////////////////// RESERVAS ///////////////////////////////
+
+/****** Object:  StoredProcedure [dbo].[SP_AgregarReserva] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[SP_AgregarReserva]
+(
+	@idmedico BIGINT,
+	@idPaciente BIGINT,
+	@idhorario BIGINT,
+	@idobservacion VARCHAR(350)
+)
+as 
+BEGIN
+
+INSERT INTO Reserva (idMedico,idHorarioDisponible,idPaciente,observacion,fechaCreacionReserva,estado)
+VALUES (@idmedico,@idhorario,@idPaciente,@idobservacion,GETDATE(),1)
+	EXEC SP_BajaHorario @idhorario
+END
+
+/****** Object:  StoredProcedure [dbo].[SP_EliminarReserva] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[SP_EliminarReserva]
+(
+	@idReserva BIGINT
+)
+AS 
+BEGIN
+
+DECLARE @idHorario BIGINT
+SELECT @idHorario = idHorarioDisponible FROM Reserva WHERE idReserva = @idReserva
+
+DELETE FROM Reserva WHERE idReserva = @idReserva
+
+EXEC SP_AltaHorario @idHorario
+
+END
+
+
