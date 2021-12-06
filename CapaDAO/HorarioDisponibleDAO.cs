fdf7f1bfb447a88d74fca554af07480df10db098
nicolaslopez82 <nicolaslopez82@gmail.com>
2021-12-06 01:15:48 -0300

    medicos horarios disponibles backend dropdownlists

---
+++ CapaDAO/HorarioDisponibleDAO.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDominio;
+using System.Data.SqlClient;
+using CapaAccesoDatos;
+
+namespace CapaDAO
+{
+    public class HorarioDisponibleDAO
+    {
+        #region "PATRON SINGLETON"
+        private static HorarioDisponibleDAO daoHorarioDisponible = null;
+        private HorarioDisponibleDAO() { }
+        public static HorarioDisponibleDAO getInstance()
+        {
+            if (daoHorarioDisponible == null)
+            {
+                daoHorarioDisponible = new HorarioDisponibleDAO();
+            }
+            return daoHorarioDisponible;
+        }
+        #endregion
+
+        public List<HorarioDisponible> ListarHorariosDisponiblesPorMedico(int idMedico)
+        {
+            // HorarioDisponible = HD
+            List<HorarioDisponible> HDLista = new List<HorarioDisponible>();
+            SqlConnection con = null;
+            SqlCommand cmd = null;
+            SqlDataReader dr = null;
+
+            try
+            {
+                con = Conexion.getInstance().ConexionBD();
+                cmd = new SqlCommand("SP_ListarHorariosDisponiblesPorMedico", con);
+                cmd.Parameters.AddWithValue("@idMedico", idMedico);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                con.Open();
+
+                dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    HorarioDisponible objHorarioDisponible = new HorarioDisponible();
+                    {
+                        objHorarioDisponible.IdHorarioDisponible = (int)dr["idHorarioDisponible"];
+                        objHorarioDisponible.FechaHorarioDisponible = (DateTime)dr["fechaHorarioDisponible"];                   
+                    }
+                    HDLista.Add(objHorarioDisponible);
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return HDLista;
+
+        }
+    }
+}
